55a36bb083e5b9d10f5526871d6ea60f1ac15ba0
truongbathuong <thuonghalong111@gmail.com>
2021-01-27 10:58:35 +0700

    Java

---
+++ Java/Ht.java
@@ -0,0 +1,21 @@
+package Java;
+
+import java.util.Scanner;
+public class Ht 
+{
+        public static double tinhDienTich(double r) {
+            return r * r * 3.14;
+        }
+        public static double tinhChuVi(double r){
+            return r * 2 * 3.14;
+        }
+        public static void main(String[] args) {
+            double r;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Nhập bán kính hình tròn: ");
+            r = scanner.nextDouble();
+            System.out.println("Diện tích hình tròn là: " + tinhDienTich(r));
+            System.out.println("Chu vi hình tròn là: "+ tinhChuVi(r));
+            System.out.println("----------------------------");
+        }
+    }
