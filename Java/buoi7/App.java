39ee1840e73e84e2ec91126ab5fcc32e94779da9
truongbathuong <thuonghalong111@gmail.com>
2021-03-05 19:02:47 +0700

    update

---
+++ Java/buoi7/App.java
@@ -0,0 +1,10 @@
+package Java111.buoi7;
+
+public class App {
+    public static void main(String[] args) {
+        Students st = new Students();
+        st.input();
+        st.display();
+    }
+    
+}
