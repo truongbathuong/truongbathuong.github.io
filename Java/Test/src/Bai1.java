2cc20703f51fb6a57dd924416443e5b0877c390c
truongbathuong <thuonghalong111@gmail.com>
2021-03-28 21:52:05 +0700

    update

---
+++ Java/Test/src/Bai1.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+     class bai1 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            ArrayList<Integer> list = new ArrayList<Integer>();
+            System.out.println("Nhap ki tu: ");
+            for (int i = 0; i < 100; i++) {
+                String n = scanner.nextLine();
+                if (n.toUpperCase().equals("Q")) {
+                    break;
+                }
+                if (n.matches("^[-+]?\\d+$")) {
+                    list.add(Integer.valueOf(n));
+                }
+            }
+            scanner.close();
+            Collections.sort(list);
+            System.out.println("Mang da sap xep: ");
+            list.forEach(System.out::println);
+
+        }
+    }
+
+
+
---
+++ Java/Test/src/bai2.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class bai2 {
+    public static void main(String[] args) {
+        System.out.println("Hãy nhập số N nguyên dương. ĐK: 8< N < 14");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Nhap N: ");
+        int n = Integer.valueOf(scan.nextLine());
+
+        // tao mang ngau nhien n phan tu
+        int[] createdNumber = randomNumbers(n);
+        System.out.println("Mang ngau nhien la: " + Arrays.toString(createdNumber));
+
+        // Nhap so X
+        System.out.print("Nhập số X trong khoảng 10 đến -10: ");
+        int target = Integer.valueOf(scan.nextLine());
+        scan.close();
+
+    }
+
+    public static int[] randomNumbers(int n) {
+        int[] randomNumbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            randomNumbers[i] = (int) (Math.random() * 21) - 10;
+        }
+        return randomNumbers;
+    }
+
+
+}
