89ed8ea633aeecf2be2c74d70ca7ffe4da2718aa
truongbathuong <76955363+truongbathuong@users.noreply.github.com>
2023-03-26 11:28:16 +0700

    Add files via upload

---
+++ thiep-mung/config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = '#';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Chị';
+const contentCard = 'Chúc chị yêu của em sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với chị trong hôm nay và cả những ngày sau';
+
+const giftImage = 'nana.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
